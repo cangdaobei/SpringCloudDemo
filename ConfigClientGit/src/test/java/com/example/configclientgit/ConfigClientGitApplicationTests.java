27ad04b27547e770add91f0d6648dd4da23a39a0
zhangchuangrong <zhang19951007>
2022-03-03 00:10:08 +0800

    SpringCloudConfig服务端使用git配置提交

---
+++ ConfigClientGit/src/test/java/com/example/configclientgit/ConfigClientGitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.configclientgit;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ConfigServerGit/src/test/java/com/example/configservergit/ConfigServerGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservergit;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
